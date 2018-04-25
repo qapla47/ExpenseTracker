a2c513025c04522bdcab8c41dff499ea005945a2
Teresa Strout <qapla47@gmail.com>
2018-04-25 14:17:41 -0400

    setup production webpack build

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
-import "react-dates/lib/css/_datepicker.css";
-
 import configureStore from "./store/configureStore";
 
 import { addExpense, editExpense, removeExpense } from "./actions/expenses";
@@ -15,6 +13,7 @@ import AppRouter from "./routers/AppRouter";
 // normalize will reset the browser to enforce cross browser compatibility
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
+import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
 
