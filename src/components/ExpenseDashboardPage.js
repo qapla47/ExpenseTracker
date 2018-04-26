a55a4d142422a7b5894ef7071149b675938558c4
Teresa Strout <qapla47@gmail.com>
2018-04-26 18:09:47 -0400

    added a summary of expenses, changes for filters

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -4,9 +4,11 @@ import ReactDOM from "react-dom";
 
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './expenseListFilters';
+import ExpensesSummary from './ExpensesSummary';
 
 const ExpenseDashboardPage = () => (
   <div>
+    <ExpensesSummary />
     <ExpenseListFilters />
     <ExpenseList />
   </div>
